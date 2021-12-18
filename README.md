b058c50a614a648dd2e98a1fd82a5d9b2d0f1912
Clayton Snyder <snyderc@gmx.com>
2021-12-18 14:06:47 -0800

    tidy

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 https://github.com/bwmarrin/discordgo
+
 https://github.com/Spotifyd/spotifyd
+
 https://github.com/gordonklaus/portaudio
 
+    go build
+    nohup ./brock -t <discord token> -m "Greeting message" > botlog &
--- main.go
+++ main.go
@@ -1,105 +1,100 @@
 package main
 
 import (
-    "flag"
-    "os/signal"
-    "os"
-    "fmt"
-    "math/rand"
-    "syscall"
-    "time"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	// "math/rand"
+	"syscall"
+	// "time"
 
-    "github.com/bwmarrin/discordgo"
-//    "github.com/zmb3/spotify/v2"
-    "github.com/gordonklaus/portaudio"
+	"github.com/bwmarrin/discordgo"
+	// "github.com/gordonklaus/portaudio"
 )
 
 var (
-    DiscordToken        string
-    SpotifyClientID     string
-    SpotifyClientSecret string
-    GreetingMsg         string
-    DEBUG_AudioFilePath string
+	DiscordToken string
+	GreetingMsg  string
 )
 
 func init() {
-    flag.StringVar(&DiscordToken, "discord-token", "OTE5NDU1ODQyNTkwNDI1MTA4.YbWD-w.cnrThGTgu3E7V0Bztjs0DnUag8E", "")
-    flag.StringVar(&SpotifyClientID, "spotify-client-id", "", "")
-    flag.StringVar(&SpotifyClientSecret, "spotify-client-secret", "", "")
+	flag.StringVar(&DiscordToken, "t", "", "")
+	flag.StringVar(&GreetingMsg, "m", "I believe in rock-hard defense and determination!", "")
+	flag.Parse()
 
-    flag.StringVar(&DEBUG_AudioFilePath, "audio-file", "", "")
-    flag.Parse()
-
-    fmt.Printf("Flags: DiscordToken=%v, SpotifyClientID=%v, SpotifyClientSecret=%v\n",
-        DiscordToken, SpotifyClientID, SpotifyClientSecret)
+	fmt.Printf("Flags: DiscordToken=%v, GreetingMsg=%v\n", DiscordToken, GreetingMsg)
 }
 
 func main() {
-    // Create Discord session with token from flag
-    dg, err := discordgo.New("Bot " + DiscordToken)
+	// Create Discord session with token from flag
+	dg, err := discordgo.New("Bot " + DiscordToken)
 
-    if err != nil {
-        fmt.Printf("Error creating Discord session: '%v'\n", err)
-        return
-    }
+	if err != nil {
+		fmt.Printf("Error creating Discord session: '%v'\n", err)
+		return
+	}
 
-    dg.AddHandler(handleMessage)
+	dg.AddHandler(handleMessage)
 
-    dg.Identify.Intents = discordgo.IntentsGuildMessages
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
-    err = dg.Open()
-    if err != nil {
-        fmt.Printf("Error with dg.Open(): '%v'\n", err)
-        return
-    }
+	err = dg.Open()
+	if err != nil {
+		fmt.Printf("Error with dg.Open(): '%v'\n", err)
+		return
+	}
 
-    fmt.Printf("Bot runnin'.")
+	fmt.Printf("Bot runnin'.")
 
-
-    //f, err := os.Open(DEBUG_AudioFilePath)
-    //if err != null {
-    //    fmt.Printf("Error reading audio file: '%v' (DEBUG_AudioFilePath=%v)\n",
-    //        err, DEBUG_AudioFilePath)
-    //}
-
-    portaudio.Initialize()
-    defer portaudio.Terminate()
-    h, err := portaudio.DefaultHostApi()
-    if err != nil {
-        fmt.Printf("Error with portaudio.DefaultHostApi(): '%v'\n", err)
-        return
-    }
-
-    stream, err := portaudio.OpenStream(portaudio.HighLatencyParameters(nil, h.DefaultOutputDevice), func(out []int32) {
-        for i := range out {
-            out[i] = int32(rand.Uint32())
+    // we're gonna blast a welcome message to every channel we can
+    for _, guild := range dg.State.Guilds {
+        channels, _ := dg.GuildChannels(guild.ID)
+        for _, channel := range channels {
+            if channel.Type == discordgo.ChannelTypeGuildText {
+                dg.ChannelMessageSend(channel.ID, GreetingMsg)
+            }
         }
-    })
-    if err != nil {
-        fmt.Printf("Error with portaudio.OpenStream(): '%v'\n", err)
-        return
     }
 
-    defer stream.Close()
-    stream.Start()
-    time.Sleep(time.Second)
-    stream.Stop()
+	// portaudio.Initialize()
+	// defer portaudio.Terminate()
+	// h, err := portaudio.DefaultHostApi()
+	// if err != nil {
+	//     fmt.Printf("Error with portaudio.DefaultHostApi(): '%v'\n", err)
+	//     return
+	// }
 
-    sig := make(chan os.Signal, 1)
-    signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-    <-sig
+	// stream, err := portaudio.OpenStream(portaudio.HighLatencyParameters(nil, h.DefaultOutputDevice), func(out []int32) {
+	//     for i := range out {
+	//         out[i] = int32(rand.Uint32())
+	//     }
+	// })
+	// if err != nil {
+	//     fmt.Printf("Error with portaudio.OpenStream(): '%v'\n", err)
+	//     return
+	// }
 
-    dg.Close()
+	// defer stream.Close()
+	// stream.Start()
+	// time.Sleep(time.Second)
+	// stream.Stop()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sig
+
+	dg.Close()
 }
 
 func handleMessage(session *discordgo.Session, msg *discordgo.MessageCreate) {
-    // Don't talk to yourself
-    if msg.Author.ID == session.State.User.ID {
-        return
-    }
+	// Don't talk to yourself
+	if msg.Author.ID == session.State.User.ID {
+		return
+	}
 
-    if msg.Content == "bing" {
-        fmt.Printf("%v: %v", msg.Author.Username, msg.Content)
-        session.ChannelMessageSend(msg.ChannelID, "bong")
-    }
+	if msg.Content == "bing" {
+		fmt.Printf("%v: %v", msg.Author.Username, msg.Content)
+		session.ChannelMessageSend(msg.ChannelID, "bong")
+	}
 }
